87b8cf0039142558aa75eea33f41e6651d7c9fd1
kale <jinkai.jk@alibaba-inc.com>
2023-06-11 10:58:01 +0800

    将gadget-template变成submodule模式

--- gadgets/template/README.md
+++
@@ -1,2 +0,0 @@
-npx tsc src/plugin.ts
-
--- gadgets/template/src/View.tsx
+++
@@ -1,9 +0,0 @@
-import { IViewElementProps, ActionInfoType } from '../Interface'
-
-export default (
-  {
-    viewType, data, expectation, onSendAction: sendAction,
-  }: IViewElementProps & { onSendAction: (actionInfo: ActionInfoType) => void },
-) => {
-  return viewType === 'md5Text' ? <span>{data.md5Str}</span> : <div />
-}
