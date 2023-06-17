b4cac68c2564000a4d3df7bf9b93837cefd50946
Kale <460412008@qq.com>
2023-06-18 01:09:51 +0800

    支持ggithub登录

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/reset.css';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { KEY } from './constant'
+import 'antd/dist/reset.css'
+import './index.css'
+import App from './App'
+
+const queries = new URLSearchParams(window.location.search)
+
+// 持久化access_token
+const accessToken = queries.get('access_token')
+if (accessToken && accessToken !== 'undefined') {
+  localStorage.setItem(KEY.ACCESS_TOKEN, accessToken)
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
